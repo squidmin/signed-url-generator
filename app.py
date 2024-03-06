cd1a575d3c66adc5a71f74457b2097bf46b31442
squidmin <morse.james.r@gmail.com>
2024-03-05 18:02:38 -0600

    Set CORS headers (cont'd)

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ app = Flask(__name__)
 # This enables CORS for all domains on all routes. For high-stakes production deployments,
 # you might want to restrict this.
 # CORS(app)
-CORS(app, supports_credentials=True)
+# CORS(app, supports_credentials=True)
+CORS(app, resources={r"*": {"origins": "*", "allow_headers": "*"}})
 # CORS(app, resources={r"/generate-signed-url": {"origins": "http://localhost:3000"}}, supports_credentials=True)
 
 
